3a52e210bb3e63387c975ea2f222274082d3ab84
Splendorg <splendorg@protonmail.com>
2024-12-09 15:20:31 +0300

    fixed eslint

--- components/Restaurant/NotFound.tsx
+++ components/Restaurant/NotFound.tsx
@@ -13,7 +13,7 @@ const NoRestaurantFound = () => {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="text-center p-6">
                 <h1 className="text-4xl font-bold text-gray-800 mb-4">Restaurant Not Found</h1>
-                <p className="text-lg text-gray-600 mb-6">Unfortunately, we couldn't find the restaurant you were looking for. Please try again with a different search.</p>
+                <p className="text-lg text-gray-600 mb-6">Unfortunately, we couldn&apos;t find the restaurant you were looking for. Please try again with a different search.</p>
                 <button
                     onClick={handleGoBack}
                     className="px-6 py-3 text-white bg-blue-600 hover:bg-blue-700 rounded-lg transition-all duration-200"
